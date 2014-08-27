5121dfe0f0eaab5013932ced66d3575ccd299b71
azangru <andrey.azov@gmail.com>
2014-08-27 18:35:45 +0400

    Initial commit (rails 3.2.17 app, added devise engine)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestRegistration::Application.config.secret_token = 'b65becf24dcd43adaba0283dbd0ff4d7c41a9a5a1424c0612d56bf40bda9cdca1f1d9bae004188a9458e4e7d4236aea9e442c600ee6599805a19d81f496f188b'
